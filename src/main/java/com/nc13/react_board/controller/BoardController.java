6195a390c597b4db7d2f86473df68837f2649626
BIT\BITCAMP <wjdwltn0909@gmail.com>
2024-07-30 15:19:16 +0900

    commit 0730

--- src/main/java/com/nc13/react_board/controller/BoardController.java
+++ src/main/java/com/nc13/react_board/controller/BoardController.java
@@ -3,8 +3,13 @@ package com.nc13.react_board.controller;
 import com.nc13.react_board.model.BoardDTO;
 import com.nc13.react_board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -20,5 +25,48 @@ public class BoardController {
     @GetMapping("showOne/{id}")
     public BoardDTO selectOne(@PathVariable int id) {
         return BOARD_SERVICE.selectOne(id);
+    }
+
+    @GetMapping("showList/{pageNo}")
+    public HashMap<String, Object> selectList(@PathVariable int pageNo) {
+        HashMap<String, Object> resultMap = new HashMap<>();
+
+        int maxPage = BOARD_SERVICE.selectMaxPage();
+
+        int startPage = 1;
+        int endPage = 1;
+
+        if(maxPage < 5) {
+            endPage = maxPage;
+        } else if (pageNo <= 3) {
+            endPage = 5;
+        } else if (pageNo >= maxPage - 2) {
+            startPage = maxPage - 4;
+            endPage = maxPage;
+        } else {
+            startPage = pageNo - 2;
+            endPage = pageNo + 2;
+        }
+
+        resultMap.put("currentPage", pageNo);
+        resultMap.put("startPage", startPage);
+        resultMap.put("endPage", endPage);
+        resultMap.put("maxPage", maxPage);
+        resultMap.put("boardList", BOARD_SERVICE.selectAll(pageNo));
+        return resultMap;
+    }
+
+    @PostMapping("write")
+    public HashMap<String, Object>  write(@RequestBody BoardDTO boardDTO) {
+        HashMap<String, Object> resultMap = new HashMap();
+        try {
+            BOARD_SERVICE.insert(boardDTO);
+            resultMap.put("result", "success");
+            resultMap.put("resultId", boardDTO.getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+            resultMap.put("result", "fail");
+        }
+        return resultMap;
     }
 }
--- src/main/java/com/nc13/react_board/service/BoardService.java
+++ src/main/java/com/nc13/react_board/service/BoardService.java
@@ -5,10 +5,14 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class BoardService {
     private final SqlSession SESSION;
     private final String NAMESPACE = "mappers.BoardMapper";
+    private final int PAGE_SIZE = 20;
 
     @Autowired
     public BoardService(SqlSession session) {
@@ -17,5 +21,25 @@ public class BoardService {
 
     public BoardDTO selectOne(int id) {
         return SESSION.selectOne(NAMESPACE + ".selectOne", id);
+    }
+
+    public List<BoardDTO> selectAll(int pageNo) {
+        HashMap<String, Integer> paramMap = new HashMap<>();
+        paramMap.put("startRow", (pageNo - 1)*PAGE_SIZE);
+        paramMap.put("size", PAGE_SIZE);
+        return SESSION.selectList(NAMESPACE + ".selectList", paramMap);
+    }
+
+    public int selectMaxPage() {
+        int maxRow = SESSION.selectOne(NAMESPACE + ".selectMaxPage");
+        int maxPage = maxRow / PAGE_SIZE;
+
+        maxPage = (maxPage % PAGE_SIZE == 0)? maxPage: maxPage + 1;
+
+        return maxPage;
+    }
+
+    public void insert(BoardDTO boardDTO) {
+        SESSION.insert(NAMESPACE + ".insert", boardDTO);
     }
 }
